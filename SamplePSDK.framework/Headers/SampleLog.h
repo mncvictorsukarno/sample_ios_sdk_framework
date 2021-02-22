13c0ef66fc88ab92c50b2fad728f887af69dc300
Victor Agung Sukarno <victor.sukarno@mncgroup.com>
2021-02-22 11:26:15 +0700

    no message

---
+++ SamplePSDK.framework/Headers/SampleLog.h
@@ -0,0 +1,19 @@
+//
+//  SampleLog.h
+//  SamplePSDK
+//
+//  Created by Victor Toya on 22/02/21.
+//  Copyright © 2021 MNC Innovation Center. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SampleLog : NSObject
+
+- (void)showLogWithString:(NSString *)string andKey:(NSString *)key;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SamplePSDK.framework/Headers/SamplePSDK.h
@@ -0,0 +1,19 @@
+//
+//  SamplePSDK.h
+//  SamplePSDK
+//
+//  Created by Victor Toya on 22/02/21.
+//  Copyright © 2021 MNC Innovation Center. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SamplePSDK.
+FOUNDATION_EXPORT double SamplePSDKVersionNumber;
+
+//! Project version string for SamplePSDK.
+FOUNDATION_EXPORT const unsigned char SamplePSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SamplePSDK/PublicHeader.h>
+
+
